deb1b375450685beb19783fa928033de3ac2749d
timeless%mac.com <>
2002-06-04 06:04:44 +0000

    Bug 148674 Boolean Charts don't work in Netpositive because '-' is sent as '%2D This makes CGI.pl closer to CGI.pm by having it unescape the name field in addition to the value field. r=preed,justdave

--- CGI.pl
+++ CGI.pl
@@ -109,10 +109,10 @@ sub ParseUrlString {
 	my $name;
 	my $value;
 	if ($item =~ /^([^=]*)=(.*)$/) {
-	    $name = $1;
+	    $name = url_decode($1);
 	    $value = url_decode($2);
 	} else {
-	    $name = $item;
+	    $name = url_decode($item);
 	    $value = "";
 	}
 	if ($value ne "") {

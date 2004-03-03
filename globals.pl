caf2b2bedf74954c0b7e63f29212b2bbaacce2ca
justdave%syndicomm.com <>
2004-03-03 15:28:16 +0000

    Bump version number to 2.16.5 for release.

--- globals.pl
+++ globals.pl
@@ -98,7 +98,7 @@ $::SIG{TERM} = 'IGNORE';
 $::SIG{PIPE} = 'IGNORE';
 
 # Contains the version string for the current running Bugzilla.
-$::param{'version'} = '2.16.4';
+$::param{'version'} = '2.16.5';
 
 $::dontchange = "--do_not_change--";
 $::chooseone = "--Choose_one:--";

1ec1d6e024991ed6ed3f37fc48bd4dfd9c7cff58
mkanat%bugzilla.org <>
2006-04-23 12:23:53 +0000

    Bump version post-release.

--- Bugzilla/Config.pm
+++ Bugzilla/Config.pm
@@ -93,7 +93,7 @@ our @parampanels = ();
 Exporter::export_ok_tags('admin', 'db', 'locations', 'params');
 
 # Bugzilla version
-$Bugzilla::Config::VERSION = "2.22";
+$Bugzilla::Config::VERSION = "2.22+";
 
 use Safe;
 

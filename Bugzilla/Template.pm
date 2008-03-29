acd4dd5e3a5a7ba8e554158a8e5829b893f51404
lpsolit%gmail.com <>
2008-03-29 04:54:14 +0000

    Bug 425746: Bugzilla::Template::precompile_templates() doesn't flush template_include_path_ correctly - Patch by FrÃ©dÃ©ric Buclin <LpSolit@gmail.com> r/a=mkanat

--- Bugzilla/Template.pm
+++ Bugzilla/Template.pm
@@ -922,6 +922,9 @@ sub precompile_templates {
 
     # If anything created a Template object before now, clear it out.
     delete Bugzilla->request_cache->{template};
+    # This is the single variable used to precompile templates,
+    # which needs to be cleared as well.
+    delete Bugzilla->request_cache->{template_include_path_};
 }
 
 # Helper for precompile_templates

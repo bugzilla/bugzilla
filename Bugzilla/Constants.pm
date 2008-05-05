04304ffd4c4efb51d47cf7b3faa56ac91581e3f7
mkanat%bugzilla.org <>
2008-05-05 06:57:17 +0000

    Bump version number for 3.0.4 release.

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -147,7 +147,7 @@ use File::Basename;
 # CONSTANTS
 #
 # Bugzilla version
-use constant BUGZILLA_VERSION => "3.0.3+";
+use constant BUGZILLA_VERSION => "3.0.4";
 
 #
 # ControlMap constants for group_control_map.

97bca1b6bdc1ff1830738214f30978c9a8c1ed11
lpsolit%gmail.com <>
2008-09-22 03:03:54 +0000

    Bug 340609: ValidateBugId() should throw an error if no bug ID is passed - Patch by FrÃ©dÃ©ric Buclin <LpSolit@gmail.com> r=bkor a=LpSolit

--- Bugzilla/Bug.pm
+++ Bugzilla/Bug.pm
@@ -2003,6 +2003,8 @@ sub ValidateBugID {
     my $dbh = Bugzilla->dbh;
     my $user = Bugzilla->user;
 
+    ThrowUserError('invalid_bug_id_or_alias', { field => $field }) unless defined $id;
+
     # Get rid of leading '#' (number) mark, if present.
     $id =~ s/^\s*#//;
     # Remove whitespace

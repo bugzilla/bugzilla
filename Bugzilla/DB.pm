8dc59ccd1e9fc846efe991327f18b1538be8fc2c
mkanat%bugzilla.org <>
2008-11-05 23:29:49 +0000

    Oh, actually, Xiaoou was right about where the constraint is supposed to go.

--- Bugzilla/DB.pm
+++ Bugzilla/DB.pm
@@ -680,11 +680,11 @@ sub bz_add_field_tables {
     my ($self, $field) = @_;
     
     $self->_bz_add_field_table($field->name,
-                                $self->_bz_schema->FIELD_TABLE_SCHEMA);
+                               $self->_bz_schema->FIELD_TABLE_SCHEMA, $field->type);
     if ($field->type == FIELD_TYPE_MULTI_SELECT) {
         my $ms_table = "bug_" . $field->name;
         $self->_bz_add_field_table($ms_table,
-            $self->_bz_schema->MULTI_SELECT_VALUE_TABLE, $field->type);
+            $self->_bz_schema->MULTI_SELECT_VALUE_TABLE);
 
         $self->bz_add_fk($ms_table, 'bug_id', {TABLE => 'bugs',
                                                COLUMN => 'bug_id',

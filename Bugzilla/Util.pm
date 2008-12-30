9386b777aa8f68fa3d2d2fdaacdb5f2846cb845b
lpsolit%gmail.com <>
2008-12-30 01:40:36 +0000

    Bug 105960: xml.cgi generates invalid XML - Patch by Kip Hampton <khampton@totalcinema.com> r/a=mkanat

--- Bugzilla/Util.pm
+++ Bugzilla/Util.pm
@@ -199,6 +199,16 @@ sub xml_quote {
     $var =~ s/>/\&gt;/g;
     $var =~ s/\"/\&quot;/g;
     $var =~ s/\'/\&apos;/g;
+    
+    # the following nukes characters disallowed by the XML 1.0
+    # spec, Production 2.2. 1.0 declares that only the following 
+    # are valid:
+    # (#x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF])
+    $var =~ s/([\x{0001}-\x{0008}]|
+               [\x{000B}-\x{000C}]|
+               [\x{000E}-\x{0019}]|
+               [\x{D800}-\x{DFFF}]|
+               [\x{FFFE}-\x{FFFF}])//gx;
     return $var;
 }
 

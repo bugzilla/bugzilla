4b7a15f86167781110e8b7fba17940766a528fff
mkanat%bugzilla.org <>
2009-09-11 17:10:45 +0000

    Bump the version number for 3.0.9.

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -147,7 +147,7 @@ use File::Basename;
 # CONSTANTS
 #
 # Bugzilla version
-use constant BUGZILLA_VERSION => "3.0.8+";
+use constant BUGZILLA_VERSION => "3.0.9";
 
 #
 # ControlMap constants for group_control_map.

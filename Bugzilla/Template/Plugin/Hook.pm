9d201119cce9bc16ef5e1791b3e6c4cf5f4ea604
Max Kanat-Alexander <mkanat@bugzilla.org>
2010-02-17 14:35:29 -0800

    Bug 545260: In the code for template hooks, the cached code of a hook wasn't being stored properly per-language. r=mkanat, a=mkanat (module owner)

--- Bugzilla/Template/Plugin/Hook.pm
+++ Bugzilla/Template/Plugin/Hook.pm
@@ -62,7 +62,7 @@ sub process {
     # Get the hooks out of the cache if they exist. Otherwise, read them
     # from the disk.
     my $cache = Bugzilla->request_cache->{template_plugin_hook_cache} ||= {};
-    my $lang = $cache->{language} || '';
+    my $lang = Bugzilla->request_cache->{language} || '';
     $cache->{"${lang}__$extension_template"} 
         ||= $self->_get_hooks($extension_template);
 

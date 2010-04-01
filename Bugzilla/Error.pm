11e17befd5f484859eea97b5bf831d220ce2b2a8
Max Kanat-Alexander <mkanat@bugzilla.org>
2010-03-31 18:05:14 -0700

    Bug 554714: Make JSON-RPC throw errors properly even if they are thrown before method evaluation (like during checking of Bugzilla_login and Bugzilla_password) r=dkl, a=mkanat

--- Bugzilla/Error.pm
+++ Bugzilla/Error.pm
@@ -127,9 +127,12 @@ sub _throw_error {
                                      message => $message,
                                      id      => $server->{_bz_request_id},
                                      version => $server->version);
-                # We die with no message. JSON::RPC checks raise_error before
+                # Most JSON-RPC Throw*Error calls happen within an eval inside
+                # of JSON::RPC. So, in that circumstance, instead of exiting,
+                # we die with no message. JSON::RPC checks raise_error before
                 # it checks $@, so it returns the proper error.
-                die;
+                die if _in_eval();
+                $server->response($server->error_response_header);
             }
         }
     }

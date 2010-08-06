3b806fcbedbcb4835767ac6892c26ef8781e5ef2
Max Kanat-Alexander <mkanat@bugzilla.org>
2010-08-05 18:12:06 -0700

    Bump the version number for 3.7.3.
    https://bugzilla.mozilla.org/show_bug.cgi?id=580206

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -197,7 +197,7 @@ use File::Basename;
 # CONSTANTS
 #
 # Bugzilla version
-use constant BUGZILLA_VERSION => "3.7.2+";
+use constant BUGZILLA_VERSION => "3.7.3";
 
 # These are unique values that are unlikely to match a string or a number,
 # to be used in criteria for match() functions and other things. They start

ae5aaada36f45bd9a33599e5eac02843af8a31e2
Reed Loden <reed@reedloden.com>
2011-01-21 13:14:36 -0800

    Bug 591165: (CVE-2010-4411) [SECURITY] Bump minimum required version of CGI.pm to v3.51 in order to address header injection vulnerability. [r=mkanat a=mkanat]

--- Bugzilla/Install/Requirements.pm
+++ Bugzilla/Install/Requirements.pm
@@ -94,9 +94,9 @@ sub REQUIRED_MODULES {
     {
         package => 'CGI.pm',
         module  => 'CGI',
-        # 3.50 fixes a security problem that affects Bugzilla.
+        # 3.51 fixes a security problem that affects Bugzilla.
         # (bug 591165)
-        version => '3.50',
+        version => '3.51',
     },
     {
         package => 'Digest-SHA',

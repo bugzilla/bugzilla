8f15f58aa5a6330272a3f98953bb25acdec5ac7b
Max Kanat-Alexander <mkanat@bugzilla.org>
2011-01-24 17:49:09 -0800

    Bump the version number post-release.

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -160,7 +160,7 @@ use File::Basename;
 # CONSTANTS
 #
 # Bugzilla version
-use constant BUGZILLA_VERSION => "3.2.10";
+use constant BUGZILLA_VERSION => "3.2.10+";
 
 # These are unique values that are unlikely to match a string or a number,
 # to be used in criteria for match() functions and other things. They start

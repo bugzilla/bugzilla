34c969c2144e5910e7532946d0a0313f48d31f01
Dave Lawrence <dlawrence@mozilla.com>
2012-01-11 15:08:16 -0500

    Backed out mistaken commit of patch from bug 714370

--- Bugzilla/Flag.pm
+++ Bugzilla/Flag.pm
@@ -84,8 +84,6 @@ use constant DB_COLUMNS => qw(
     type_id
     bug_id
     attach_id
-    creation_date
-    modification_date
     requestee_id
     setter_id
     status
@@ -134,14 +132,6 @@ Returns the ID of the attachment this flag belongs to, if any.
 
 Returns the status '+', '-', '?' of the flag.
 
-=item C<creation_date>
-
-Returns the timestamp when the flag was created.
-
-=item C<modification_date>
-
-Returns the timestamp when the flag was last modified.
-
 =back
 
 =cut
@@ -154,8 +144,6 @@ sub attach_id    { return $_[0]->{'attach_id'};    }
 sub status       { return $_[0]->{'status'};       }
 sub setter_id    { return $_[0]->{'setter_id'};    }
 sub requestee_id { return $_[0]->{'requestee_id'}; }
-sub creation_date     { return $_[0]->{'creation_date'};     }
-sub modification_date { return $_[0]->{'modification_date'}; }
 
 ###############################
 ####       Methods         ####
@@ -462,7 +450,6 @@ sub update {
     if (scalar(keys %$changes)) {
         $dbh->do('UPDATE flags SET modification_date = ? WHERE id = ?',
                  undef, ($timestamp, $self->id));
-        $self->{'modification_date'} = $timestamp;
     }
     return $changes;
 }

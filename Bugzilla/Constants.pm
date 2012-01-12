488547ec68fcc3de4a0fb63d469861ca74392ec9
Byron Jones <bjones@mozilla.com>
2012-01-12 15:29:26 +0800

    Bug 717502: remove pdf from bugzilla/constants

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -456,7 +456,6 @@ use constant contenttypes =>
    "csv"  => "text/csv" ,
    "png"  => "image/png" ,
    "ics"  => "text/calendar" ,
-   "pdf"  => "application/pdf" ,
   };
 
 # Usage modes. Default USAGE_MODE_BROWSER. Use with Bugzilla->usage_mode.

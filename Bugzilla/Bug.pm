4c8334f9563b88df6bc1603804f9f2d6c2b13b7a
Koosha Khajeh Moogahi <koosha.khajeh@gmail.com>
2012-07-04 00:56:31 +0200

    Bug 366152: Trim whitespaces from the URL field r/a=LpSolit

--- Bugzilla/Bug.pm
+++ Bugzilla/Bug.pm
@@ -1236,6 +1236,7 @@ sub _check_assigned_to {
 sub _check_bug_file_loc {
     my ($invocant, $url) = @_;
     $url = '' if !defined($url);
+    $url = trim($url);
     # On bug entry, if bug_file_loc is "http://", the default, use an 
     # empty value instead. However, on bug editing people can set that
     # back if they *really* want to.

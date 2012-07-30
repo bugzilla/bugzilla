4712659894b891e28d17a5475babe4c656f2060e
Matt Selsky <selsky@columbia.edu>
2012-07-30 16:51:00 -0400

    Bug 778226 - Add Mac OS 10.8 Mountain Lion detection r=timeless, a=LpSolit

--- Bugzilla/UserAgent.pm
+++ Bugzilla/UserAgent.pm
@@ -117,6 +117,7 @@ use constant OS_MAP => (
     qr/\(.*Win(?:dows[ -]|)NT.*\)/ => ["Windows NT"],
     qr/\(.*Windows.*NT.*\)/ => ["Windows NT"],
     # OS X
+    qr/\(.*Mac OS X (?:|Mach-O |\()10.8.*\)/ => ["Mac OS X 10.8"],
     qr/\(.*Mac OS X (?:|Mach-O |\()10.7.*\)/ => ["Mac OS X 10.7"],
     qr/\(.*Mac OS X (?:|Mach-O |\()10.6.*\)/ => ["Mac OS X 10.6"],
     qr/\(.*Mac OS X (?:|Mach-O |\()10.5.*\)/ => ["Mac OS X 10.5"],

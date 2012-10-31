e3153622907e9ddfb31ef980c486b4985ed577e3
Dave Lawrence <dlawrence@mozilla.com>
2012-10-31 15:52:09 -0400

    Bug 787478 - Create Custom Entry Form for Data Safety/Legal/Security/Privacy Assurance Bugs

---
+++ extensions/MozProjectReview/Extension.pm
@@ -0,0 +1,253 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# This Source Code Form is "Incompatible With Secondary Licenses", as
+# defined by the Mozilla Public License, v. 2.0.
+package Bugzilla::Extension::MozProjectReview;
+
+use strict;
+
+use base qw(Bugzilla::Extension);
+
+use Bugzilla::User;
+use Bugzilla::Group;
+use Bugzilla::Error;
+use Bugzilla::Constants;
+
+our $VERSION = '0.01';
+
+sub post_bug_after_creation {
+    my ($self, $args) = @_;
+    my $vars   = $args->{vars};
+    my $bug    = $vars->{bug};
+
+    my $user     = Bugzilla->user;
+    my $params   = Bugzilla->input_params;
+    my $template = Bugzilla->template;
+
+    return if !($params->{format}
+                && $params->{format} eq 'moz-project-review'
+                && $bug->component eq 'Project Review');
+
+    my $error_mode_cache = Bugzilla->error_mode;
+    Bugzilla->error_mode(ERROR_MODE_DIE);
+
+    # do a match if applicable
+    Bugzilla::User::match_field({
+        'legal_cc' => { 'type' => 'multi' }
+    });
+
+    my ($do_sec_review, $do_legal, $do_finance, $do_privacy_vendor,
+        $do_data_safety, $do_privacy_tech, $do_privacy_policy);
+
+    if ($params->{mozilla_data} eq 'Yes') {
+        $do_legal = 1;
+        $do_privacy_policy = 1;
+        $do_privacy_tech = 1;
+        $do_sec_review = 1;
+    }
+
+    if ($params->{mozilla_data} eq 'Yes'
+        && $params->{data_safety_user_data} eq 'Yes')
+    {
+        $do_data_safety = 1;
+    }
+
+    if ($params->{new_or_change} eq 'New') {
+        $do_legal = 1;
+        $do_privacy_policy = 1;
+    }
+    elsif ($params->{new_or_change} eq 'Existing') {
+        $do_legal = 1;
+    }
+
+    if ($params->{separate_party} eq 'Yes') {
+        $do_legal = 1;
+    }
+
+    if ($params->{data_access} eq 'Yes') {
+        $do_privacy_policy = 1;
+        $do_sec_review = 1;
+    }
+
+    if ($params->{data_access} eq 'Yes'
+        && $params->{'privacy_policy_vendor_user_data'} eq 'Yes') 
+    {
+        $do_privacy_vendor = 1;
+    }
+
+    if ($params->{vendor_cost} eq '> $25,000') {
+        $do_finance = 1;
+    }
+
+    my ($sec_review_bug, $legal_bug, $finance_bug, $privacy_vendor_bug,
+        $data_safety_bug, $privacy_tech_bug, $privacy_policy_bug);
+
+    eval {
+        if ($do_sec_review) {
+            my $bug_data = {
+                short_desc   => 'Security Review for ' . $bug->short_desc,
+                product      => 'mozilla.org',
+                component    => 'Security Assurance: Review Request',
+                bug_severity => 'normal',
+                groups       => [ 'mozilla-corporation-confidential' ],
+                keywords     => 'sec-review-needed',
+                op_sys       => 'All',
+                rep_platform => 'All',
+                version      => 'other',
+                blocked      => $bug->bug_id,
+            };
+            $sec_review_bug = _file_child_bug($bug, $vars, 'sec-review', $bug_data);
+        }
+
+        if ($do_legal) {
+            my $component;
+            if ($params->{new_or_change} eq 'New') {
+                $component = 'General';
+            }
+            elsif ($params->{new_or_change} eq 'Existing') {
+                $component = $params->{mozilla_project};
+            }
+
+            if ($params->{separate_party} eq 'Yes'
+                && $params->{relationship_type}) 
+            {
+                $component = $params->{relationship_type} eq 'unspecified'
+                             ? 'General'
+                             : $params->{relationship_type};
+            }
+
+            my $bug_data = {
+                short_desc   => 'Complete Legal Review for ' . $bug->short_desc,
+                product      => 'Legal', 
+                component    => $component,
+                bug_severity => 'normal',
+                priority     => '--',
+                groups       => [ 'legal' ],
+                op_sys       => 'All',
+                rep_platform => 'All',
+                version      => 'unspecified',
+                blocked      => $bug->bug_id,
+                cc           => $params->{'legal_cc'},
+            };
+            $legal_bug = _file_child_bug($bug, $vars, 'legal', $bug_data);
+
+        }
+
+        if ($do_finance) {
+            my $bug_data = {
+                short_desc   => 'Complete Finance Review for ' . $bug->short_desc,
+                product      => 'Finance',
+                component    => 'Purchase Request Form',
+                bug_severity => 'normal',
+                priority     => '--',
+                groups       => [ 'finance' ],
+                op_sys       => 'All',
+                rep_platform => 'All',
+                version      => 'unspecified',
+                blocked      => $bug->bug_id,
+            };
+            $finance_bug = _file_child_bug($bug, $vars, 'finance', $bug_data);
+        }
+
+        if ($do_data_safety) {
+            my $bug_data = {
+                short_desc   => 'Data Safety Review for ' . $bug->short_desc,
+                product      => 'Data Safety',
+                component    => 'General',
+                bug_severity => 'normal',
+                priority     => '--',
+                op_sys       => 'All',
+                rep_platform => 'All',
+                version      => 'unspecified',
+                blocked      => $bug->bug_id,
+            };
+            $data_safety_bug = _file_child_bug($bug, $vars, 'data-safety', $bug_data);
+        }
+
+        if ($do_privacy_tech) {
+            my $bug_data = {
+                short_desc   => 'Complete Privacy-Technical Review for ' . $bug->short_desc,
+                product      => 'mozilla.org',
+                component    => 'Security Assurance: Review Request',
+                bug_severity => 'normal',
+                priority     => '--',
+                keywords     => 'privacy-review-needed',
+                groups       => [ 'mozilla-corporation-confidential' ],
+                op_sys       => 'All',
+                rep_platform => 'All',
+                version      => 'other',
+                blocked      => $bug->bug_id,
+            };
+            $privacy_tech_bug = _file_child_bug($bug, $vars, 'privacy-tech', $bug_data);
+        }
+
+        if ($do_privacy_policy) {
+            my $bug_data = {
+                short_desc   => 'Complete Privacy-Policy Review for ' . $bug->short_desc,
+                product      => 'Privacy',
+                component    => 'Privacy Review',
+                bug_severity => 'normal',
+                priority     => '--',
+                op_sys       => 'All',
+                rep_platform => 'All',
+                version      => 'unspecified',
+                blocked      => $bug->bug_id,
+            };
+            $privacy_policy_bug = _file_child_bug($bug, $vars, 'privacy-policy', $bug_data);
+        }
+
+        if ($do_privacy_vendor) {
+            my $bug_data = {
+                short_desc   => 'Complete Privacy / Vendor Review for ' . $bug->short_desc,
+                product      => 'Privacy',
+                component    => 'Vendor Review',
+                bug_severity => 'normal',
+                priority     => '--',
+                op_sys       => 'All',
+                rep_platform => 'All',
+                version      => 'unspecified',
+                blocked      => $bug->bug_id,
+            };
+            $privacy_vendor_bug = _file_child_bug($bug, $vars, 'privacy-vendor', $bug_data);
+        }
+    };
+
+    my $error = $@;
+    Bugzilla->error_mode($error_mode_cache);
+
+    if ($error
+        || ($do_legal && !$legal_bug)
+        || ($do_sec_review && !$sec_review_bug)
+        || ($do_finance && !$finance_bug)
+        || ($do_data_safety && !$data_safety_bug)
+        || ($do_privacy_tech && !$privacy_tech_bug)
+        || ($do_privacy_policy && !$privacy_policy_bug)
+        || ($do_privacy_vendor && !$privacy_vendor_bug))
+    {
+        warn "Failed to create additional moz-project-review bugs: $error" if $error;
+        $vars->{message} = 'moz_project_review_creation_failed';
+        $vars->{message_error} = $error;
+    }
+}
+
+sub _file_child_bug {
+    my ($parent_bug, $vars, $template_suffix, $bug_data) = @_;
+    my $template = Bugzilla->template;
+    my $comment  = "";
+
+    my $full_template = "bug/create/comment-moz-project-review-$template_suffix.txt.tmpl";
+    $template->process($full_template, $vars, \$comment)
+        || ThrowTemplateError($template->error());
+
+    $bug_data->{comment} = $comment;
+    my $new_bug = Bugzilla::Bug->create($bug_data);
+
+    $parent_bug->set_all({ dependson => { add => [ $new_bug->bug_id ] }});
+    Bugzilla::BugMail::Send($new_bug->id, { changer => Bugzilla->user });
+
+    return $new_bug;
+}
+
+__PACKAGE__->NAME;
---
+++ extensions/MozProjectReview/web/js/moz_project_review.js
@@ -0,0 +1,149 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * This Source Code Form is "Incompatible With Secondary Licenses", as
+ * defined by the Mozilla Public License, v. 2.0. 
+ */
+
+YAHOO.namespace('MozProjectReview');
+
+var MPR = YAHOO.MozProjectReview;
+var Dom = YAHOO.util.Dom;
+
+MPR.required_fields = {
+    "initial_questions": {
+        "short_desc": "Please enter a value for project or feature name in the initial questions section",
+        "cc": "Please enter a value for points of contact in the initial questions section",
+        "urgency": "Please enter a value for urgency in the initial questions section",
+        "release_date": "Please enter a value for release date in the initial questions section",
+        "project_status": "Please select a value for project status in the initial questions section",
+        "mozilla_data": "Please select a value for mozilla data in the initial questions section",
+        "new_or_change": "Please select a value for new or change to existing project in the initial questions section",
+        "separate_party": "Please select a value for separate party in the initial questions section"
+    },
+    "finance_questions": {
+        "finance_purchase_what": "Please enter a value for what in the finance questions section",
+        "finance_purchase_why": "Please enter a value for why in the finance questions section",
+        "finance_purchase_risk": "Please enter a value for risk in the finance questions section",
+        "finance_purchase_alternative": "Please enter a value for alternative in the finance questions section",
+        "finance_purchase_inbudget": "Please enter a value for in budget in the finance questions section",
+        "finance_purchase_urgency": "Please select a value for urgency in the finance questions section",
+        "finance_purchase_cost": "Please enter a value for total cost in the finance questions section"
+    },
+    "legal_questions": {
+        "legal_priority": "Please select a priority for the legal questions section"
+    }
+};
+
+MPR.toggleSpecialSections = function () {
+    var mozilla_data_select = Dom.get('mozilla_data');
+    var data_access_select  = Dom.get('data_access');
+    var vendor_cost_select  = Dom.get('vendor_cost');
+
+    if (mozilla_data_select.value == 'Yes') {
+        Dom.removeClass('legal_questions', 'bz_default_hidden');
+        Dom.removeClass('privacy_policy_project_questions', 'bz_default_hidden');
+        Dom.removeClass('data_safety_questions', 'bz_default_hidden');
+        Dom.removeClass('sec_review_questions', 'bz_default_hidden');
+    }
+    else {
+        Dom.addClass('legal_questions', 'bz_default_hidden');
+        Dom.addClass('privacy_policy_project_questions', 'bz_default_hidden');
+        Dom.addClass('data_safety_questions', 'bz_default_hidden');
+        Dom.addClass('sec_review_questions', 'bz_default_hidden');
+    }
+
+    if (data_access_select.value == 'Yes' || mozilla_data_select.value == 'Yes') {
+        Dom.removeClass('sec_review_questions', 'bz_default_hidden');
+    }
+    else {
+        Dom.addClass('sec_review_questions', 'bz_default_hidden');
+    }
+
+    if (data_access_select.value == 'Yes') {
+        Dom.removeClass('privacy_policy_vendor_questions', 'bz_default_hidden');
+    }
+    else {
+        Dom.addClass('privacy_policy_vendor_questions', 'bz_default_hidden');
+    }
+
+    if (vendor_cost_select.value == '> $25,000') {
+        Dom.removeClass('finance_questions', 'bz_default_hidden');
+    }
+    else {
+        Dom.addClass('finance_questions', 'bz_default_hidden');
+    }
+}
+
+MPR.toggleVisibleById = function () {
+    var args   = Array.prototype.slice.call(arguments);
+    var select = args.shift();
+    var value  = args.shift();
+    var ids    = args;
+
+    if (typeof select == 'string') {
+        select = Dom.get(select);
+    }
+
+    for (var i = 0; i < ids.length; i++) {
+        if (select.value == value) {
+            Dom.removeClass(ids[i], 'bz_default_hidden');
+        }
+        else {
+            Dom.addClass(ids[i], 'bz_default_hidden');
+        }
+    }
+}
+
+MPR.validateAndSubmit = function () {
+    var alert_text = '';
+    var section = '';
+    for (section in MPR.required_fields) {
+        console.log("section: " + section);
+        if (!Dom.hasClass(section, 'bz_default_hidden')) {
+            var field = '';
+            for (field in MPR.required_fields[section]) {
+                console.log("field: " + field);
+                if (!MPR.isFilledOut(field)) {
+                    alert_text += MPR.required_fields[section][field] + "\n";
+                }
+            }
+        }
+    }
+
+    if (Dom.get('separate_party').value == 'Yes') {
+        if (!MPR.isFilledOut('relationship_type')) alert_text += "Please select a value for type of relationship\n";
+        if (!MPR.isFilledOut('data_access')) alert_text += "Please select a value for data access\n";
+        if (!MPR.isFilledOut('vendor_cost')) alert_text += "Please select a value for vendor cost\n";
+    }
+
+    if (alert_text) {
+        alert(alert_text);
+        return false;
+    }
+
+    return true;
+}
+
+YAHOO.util.Event.onDOMReady(function() {
+    MPR.toggleSpecialSections();
+    MPR.toggleVisibleById('new_or_change', 'Existing', 'mozilla_project_row');
+    MPR.toggleVisibleById('separate_party', 'Yes', 'initial_separate_party_questions');
+    MPR.toggleVisibleById('relationship_type', 'Vendor/Services', 'legal_sow_details_row');
+    MPR.toggleVisibleById('vendor_cost', '> $25,000', 'finance_questions');
+    MPR.toggleVisibleById('privacy_policy_project', 'Yes', 'privacy_policy_project_link_row');
+    MPR.toggleVisibleById('privacy_policy_user_data', 'Yes', 'privacy_policy_project_user_data_bug_row');
+    MPR.toggleVisibleById('privacy_policy_vendor_user_data', 'Yes', 'privacy_policy_vendor_extra');
+    MPR.toggleVisibleById('data_safety_user_data', 'Yes', 'data_safety_extra_questions');
+    MPR.toggleVisibleById('data_safety_retention', 'Yes', 'data_safety_retention_length_row');
+    MPR.toggleVisibleById('data_safety_separate_party', 'Yes', 'data_safety_separate_party_data_row');
+    MPR.toggleVisibleById('data_safety_community_visibility', 'Yes', 'data_safety_communication_channels_row');
+    MPR.toggleVisibleById('data_safety_community_visibility', 'No', 'data_safety_communication_plan_row');
+});
+
+//Takes a DOM element id and makes sure that it is filled out
+MPR.isFilledOut = function (elem_id)  {
+    var str = Dom.get(elem_id).value;
+    return str.length > 0 ? true : false;
+}

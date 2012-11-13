f19a7ed3dd00405980cf3da05e7ebcd97f4be00e
Dave Lawrence <dlawrence@mozilla.com>
2012-11-13 18:28:42 -0500

    Bump version post-release
    https://bugzilla.mozilla.org/show_bug.cgi?id=805644

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -197,7 +197,7 @@ use File::Basename;
 # CONSTANTS
 #
 # Bugzilla version
-use constant BUGZILLA_VERSION => "4.0.9";
+use constant BUGZILLA_VERSION => "4.0.9+";
 
 # These are unique values that are unlikely to match a string or a number,
 # to be used in criteria for match() functions and other things. They start

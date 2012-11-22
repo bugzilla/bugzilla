da12cec61f8c7e667b00fc5fc39c827d3593f021
Byron Jones <bjones@mozilla.com>
2012-11-22 16:23:53 +0800

    Bug 780820: Allows for multiple custom search criteria to match one field r=dkl,a=LpSolit

--- Bugzilla/Search/Clause.pm
+++ Bugzilla/Search/Clause.pm
@@ -30,6 +30,11 @@ sub children {
     return $self->{children};
 }
 
+sub update_search_args {
+    my ($self, $search_args) = @_;
+    # abstract
+}
+
 sub joiner { return $_[0]->{joiner} }
 
 sub has_translated_conditions {
@@ -71,7 +76,7 @@ sub walk_conditions {
     my ($self, $callback) = @_;
     foreach my $child (@{ $self->children }) {
         if ($child->isa('Bugzilla::Search::Condition')) {
-            $callback->($child);
+            $callback->($self, $child);
         }
         else {
             $child->walk_conditions($callback);

c87e45eef32d40e282148c733651df0d759a8b8b
Dave Lawrence <dlawrence@mozilla.com>
2013-02-25 18:00:05 -0500

    remove un-used code in ProductDashboard/lib/Util.pm

--- extensions/ProductDashboard/lib/Util.pm
+++ extensions/ProductDashboard/lib/Util.pm
@@ -17,7 +17,6 @@ use base qw(Exporter);
     closed_states
     quoted_open_states
     quoted_closed_states
-    filter_bugs
     bug_milestone_link_total
     bug_milestone_link_open
     bug_milestone_link_closed
@@ -25,8 +24,6 @@ use base qw(Exporter);
 
 use Bugzilla::Status;
 use Bugzilla::Util;
-
-use Bugzilla::Status;
 
 our $_open_states;
 sub open_states {
@@ -93,24 +90,6 @@ sub bug_milestone_link_closed {
 
     return correct_urlbase() . 'buglist.cgi?product=' . url_quote($product->name) . 
         "&target_milestone=" . url_quote($milestone->name) . "&bug_status=__closed__";
-}
-
-sub filter_bugs {
-    my ($unfiltered_bugs) = @_;
-    my $dbh = Bugzilla->dbh;
-
-    # Filter out which bugs that cannot be viewed
-    my $params = Bugzilla::CGI->new({ bug_id => [ map { $_->{'id'} } @$unfiltered_bugs ] });
-    my $search = Bugzilla::Search->new(fields => ['bug_id' ], params => $params );
-    my %filtered_bug_ids = map { $_ => 1 } @{$dbh->selectcol_arrayref($search->getSQL())};
-
-    my @filtered_bugs;
-    foreach my $bug (@$unfiltered_bugs) {
-        next if !$filtered_bug_ids{$bug->{'id'}};
-        push(@filtered_bugs, $bug);
-    }
-
-    return \@filtered_bugs;
 }
 
 1;

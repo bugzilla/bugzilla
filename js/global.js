975dd38482945e10cf0ea2f020856653298d5c28
Byron Jones <bjones@mozilla.com>
2013-07-02 11:48:27 +0800

    Bug 888806: Bugzilla login field should use "placeholder" HTML5 attribute instead of JavaScript

--- js/global.js
+++ js/global.js
@@ -16,8 +16,6 @@
 *                 
 */
 
-var mini_login_constants;
-
 function show_mini_login_form( suffix ) {
     var login_link = document.getElementById('login_link' + suffix);
     var login_form = document.getElementById('mini_login' + suffix);
@@ -67,13 +65,10 @@ function hide_forgot_form( suffix ) {
 function init_mini_login_form( suffix ) {
     var mini_login = document.getElementById('Bugzilla_login' +  suffix );
     var mini_password = document.getElementById('Bugzilla_password' +  suffix );
-    var mini_dummy = document.getElementById(
-        'Bugzilla_password_dummy' + suffix);
+    var mini_dummy = document.getElementById('Bugzilla_password_dummy' + suffix);
     // If the login and password are blank when the page loads, we display
     // "login" and "password" in the boxes by default.
     if (mini_login.value == "" && mini_password.value == "") {
-        mini_login.value = mini_login_constants.login;
-        YAHOO.util.Dom.addClass(mini_login, "bz_mini_login_help");
         YAHOO.util.Dom.addClass(mini_password, 'bz_default_hidden');
         YAHOO.util.Dom.removeClass(mini_dummy, 'bz_default_hidden');
     }
@@ -82,33 +77,15 @@ function init_mini_login_form( suffix ) {
     }
 }
 
-// Clear the words "login" and "password" from the form when you click
-// in one of the boxes. We clear them both when you click in either box
-// so that the browser's password-autocomplete can work.
-function mini_login_on_focus( suffix ) {
-    var mini_login = document.getElementById('Bugzilla_login' +  suffix );
-    var mini_password = document.getElementById('Bugzilla_password' +  suffix );
-    var mini_dummy = document.getElementById(
-        'Bugzilla_password_dummy' + suffix);
-
-    YAHOO.util.Dom.removeClass(mini_login, "bz_mini_login_help");
-    if (mini_login.value == mini_login_constants.login) {
-        mini_login.value = '';
-    }
-    YAHOO.util.Dom.removeClass(mini_password, 'bz_default_hidden');
-    YAHOO.util.Dom.addClass(mini_dummy, 'bz_default_hidden');
-}
-
 function check_mini_login_fields( suffix ) {
     var mini_login = document.getElementById('Bugzilla_login' +  suffix );
     var mini_password = document.getElementById('Bugzilla_password' +  suffix );
-    if( (mini_login.value != "" && mini_password.value != "") 
-         &&  mini_login.value != mini_login_constants.login )
-    {
-      return true;
+    if (mini_login.value != "" && mini_password.value != "") {
+        return true;
+    } else {
+        window.alert("You must provide the email address and password before logging in.");
+        return false;
     }
-    window.alert( mini_login_constants.warning );
-    return false;
 }
 
 function set_language( value ) {

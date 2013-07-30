c5fb55b45ed5ed9b51ab967ab4b1019085641294
Frédéric Buclin <LpSolit@gmail.com>
2013-07-30 11:15:15 +0200

    Bug 897029: Preload bug permissions when calling Bug.get to improve performance r/a=sgreen

--- Bugzilla/WebService/Bug.pm
+++ Bugzilla/WebService/Bug.pm
@@ -323,6 +323,12 @@ sub get {
 
     my @bugs;
     my @faults;
+
+    # Cache permissions for bugs. This highly reduces the number of calls to the DB.
+    # visible_bugs() is only able to handle bug IDs, so we have to skip aliases.
+    my @int = grep { $_ =~ /^\d+$/ } @$ids;
+    Bugzilla->user->visible_bugs(\@int);
+
     foreach my $bug_id (@$ids) {
         my $bug;
         if ($params->{permissive}) {

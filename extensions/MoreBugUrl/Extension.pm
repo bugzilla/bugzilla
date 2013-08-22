a91d06cd0e37b4c0a4e4d610c7c7bf69e4bfef08
Simon Green <sgreen@redhat.com>
2013-08-22 14:20:26 +1000

    Bug 891236 - Allow Redmine See Also URLs r=justdave, a=justdave

--- extensions/MoreBugUrl/Extension.pm
+++ extensions/MoreBugUrl/Extension.pm
@@ -18,6 +18,7 @@ use constant MORE_SUB_CLASSES => qw(
     Bugzilla::Extension::MoreBugUrl::RT
     Bugzilla::Extension::MoreBugUrl::GetSatisfaction
     Bugzilla::Extension::MoreBugUrl::PHP
+    Bugzilla::Extension::MoreBugUrl::Redmine
 );
 
 # We need to update bug_see_also table because both

57e9f86a58060ecbad9bc41d26d6998dccd11c69
Byron Jones <bjones@mozilla.com>
2013-10-16 14:05:19 +0800

    Bug 927026: searching for an unset tracking flag is failing again

--- Bugzilla/Search.pm
+++ Bugzilla/Search.pm
@@ -1669,10 +1669,6 @@ sub _params_to_data_structure {
     # And then process the modern "custom search" format.
     $clause->add( $self->_custom_search );
 
-    # BMO - allow post-processing of search clauses
-    Bugzilla::Hook::process('search_clause_structure',
-                            { search => $self, clause => $clause });
-
     return $clause;
 }
 

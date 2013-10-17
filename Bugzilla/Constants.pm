3bb60e9fd65427289d7ff8d95dffed380d940a3f
Dave Lawrence <dlawrence@mozilla.com>
2013-10-17 11:11:57 -0400

    Bump version post-release

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -182,7 +182,7 @@ use Memoize;
 # CONSTANTS
 #
 # Bugzilla version
-use constant BUGZILLA_VERSION => "4.4.1";
+use constant BUGZILLA_VERSION => "4.4.1+";
 
 # Location of the remote and local XML files to track new releases.
 use constant REMOTE_FILE => 'http://updates.bugzilla.org/bugzilla-update.xml';

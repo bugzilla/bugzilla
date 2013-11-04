36f7289d46a905f99aeb724cb65ab5ca3ae0fac0
Frédéric Buclin <LpSolit@gmail.com>
2013-11-04 19:55:45 +0100

    Bug 926952: Possible race conditions when editing or deleting a milestone or a version r/a=glob

--- Bugzilla/Milestone.pm
+++ Bugzilla/Milestone.pm
@@ -97,10 +97,12 @@ sub run_create_validators {
 
 sub update {
     my $self = shift;
+    my $dbh = Bugzilla->dbh;
+
+    $dbh->bz_start_transaction();
     my $changes = $self->SUPER::update(@_);
 
     if (exists $changes->{value}) {
-        my $dbh = Bugzilla->dbh;
         # The milestone value is stored in the bugs table instead of its ID.
         $dbh->do('UPDATE bugs SET target_milestone = ?
                   WHERE target_milestone = ? AND product_id = ?',
@@ -111,12 +113,16 @@ sub update {
                   WHERE id = ? AND defaultmilestone = ?',
                  undef, ($self->name, $self->product_id, $changes->{value}->[0]));
     }
+    $dbh->bz_commit_transaction();
+
     return $changes;
 }
 
 sub remove_from_db {
     my $self = shift;
     my $dbh = Bugzilla->dbh;
+
+    $dbh->bz_start_transaction();
 
     # The default milestone cannot be deleted.
     if ($self->name eq $self->product->default_milestone) {
@@ -146,8 +152,9 @@ sub remove_from_db {
                              Bugzilla->user->id, $timestamp);
         }
     }
-
     $self->SUPER::remove_from_db();
+
+    $dbh->bz_commit_transaction();
 }
 
 ################################

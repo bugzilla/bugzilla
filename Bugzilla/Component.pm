987e438bd66a42f693213aea4526c1136460b0b8
Frédéric Buclin <LpSolit@gmail.com>
2014-01-02 23:55:16 +0100

    Bug 895813: Mandatory custom fields whose visibility depends on a component are not required on bug creation r=pketolai r=dkl a=sgreen

--- Bugzilla/Component.pm
+++ Bugzilla/Component.pm
@@ -417,10 +417,10 @@ use constant is_default => 0;
 
 sub is_set_on_bug {
     my ($self, $bug) = @_;
-    # We treat it like a hash always, so that we don't have to check if it's
-    # a hash or an object.
-    return 0 if !defined $bug->{component_id};
-    $bug->{component_id} == $self->id ? 1 : 0;
+    my $value = blessed($bug) ? $bug->component_id : $bug->{component};
+    $value = $value->id if blessed($value);
+    return 0 unless $value;
+    return $value == $self->id ? 1 : 0;
 }
 
 ###############################

d77682e0d4213e8cc502b333cbd8e34d567445a4
Frédéric Buclin <LpSolit@gmail.com>
2014-01-16 18:22:03 +0100

    Bug 888731: Bugzilla login field should use "placeholder" HTML5 attribute instead of JavaScript r=dkl a=justdave

--- js/global.js
+++ js/global.js
@@ -6,8 +6,6 @@
  * defined by the Mozilla Public License, v. 2.0.
  */
 
-var mini_login_constants;
-
 function show_mini_login_form( suffix ) {
     var login_link = document.getElementById('login_link' + suffix);
     var login_form = document.getElementById('mini_login' + suffix);
@@ -51,53 +49,6 @@ function hide_forgot_form( suffix ) {
     YAHOO.util.Dom.removeClass(forgot_link, 'bz_default_hidden');
     YAHOO.util.Dom.addClass(forgot_form, 'bz_default_hidden');
     YAHOO.util.Dom.removeClass(login_container, 'bz_default_hidden');
-    return false;
-}
-
-function init_mini_login_form( suffix ) {
-    var mini_login = document.getElementById('Bugzilla_login' +  suffix );
-    var mini_password = document.getElementById('Bugzilla_password' +  suffix );
-    var mini_dummy = document.getElementById(
-        'Bugzilla_password_dummy' + suffix);
-    // If the login and password are blank when the page loads, we display
-    // "login" and "password" in the boxes by default.
-    if (mini_login.value == "" && mini_password.value == "") {
-        mini_login.value = mini_login_constants.login;
-        YAHOO.util.Dom.addClass(mini_login, "bz_mini_login_help");
-        YAHOO.util.Dom.addClass(mini_password, 'bz_default_hidden');
-        YAHOO.util.Dom.removeClass(mini_dummy, 'bz_default_hidden');
-    }
-    else {
-        show_mini_login_form(suffix);
-    }
-}
-
-// Clear the words "login" and "password" from the form when you click
-// in one of the boxes. We clear them both when you click in either box
-// so that the browser's password-autocomplete can work.
-function mini_login_on_focus( suffix ) {
-    var mini_login = document.getElementById('Bugzilla_login' +  suffix );
-    var mini_password = document.getElementById('Bugzilla_password' +  suffix );
-    var mini_dummy = document.getElementById(
-        'Bugzilla_password_dummy' + suffix);
-
-    YAHOO.util.Dom.removeClass(mini_login, "bz_mini_login_help");
-    if (mini_login.value == mini_login_constants.login) {
-        mini_login.value = '';
-    }
-    YAHOO.util.Dom.removeClass(mini_password, 'bz_default_hidden');
-    YAHOO.util.Dom.addClass(mini_dummy, 'bz_default_hidden');
-}
-
-function check_mini_login_fields( suffix ) {
-    var mini_login = document.getElementById('Bugzilla_login' +  suffix );
-    var mini_password = document.getElementById('Bugzilla_password' +  suffix );
-    if( (mini_login.value != "" && mini_password.value != "") 
-         &&  mini_login.value != mini_login_constants.login )
-    {
-      return true;
-    }
-    window.alert( mini_login_constants.warning );
     return false;
 }
 

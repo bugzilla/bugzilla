d05bbe563a21a61788a44bd76ebbed51f1daeb62
Gervase Markham <gerv@gerv.net>
2014-02-10 16:48:23 +0000

    Rename random stuff.css to bugzilla.css and start using it for HTML output. r=LpSolit, a=justdave.
    https://bugzilla.mozilla.org/show_bug.cgi?id=968790

--- docs/en/rst/conf.py
+++ docs/en/rst/conf.py
@@ -122,6 +122,8 @@ html_theme = 'default'
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
 
+html_style = "bugzilla.css"
+
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
 #html_logo = None

39412f01810dceff5ff92e6ffab03fb35ad71e7b
David Lawrence <dkl@mozilla.com>
2014-04-21 21:05:14 +0000

    Bumped version post-release

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -202,7 +202,7 @@ use Memoize;
 # CONSTANTS
 #
 # Bugzilla version
-use constant BUGZILLA_VERSION => "4.2.9";
+use constant BUGZILLA_VERSION => "4.2.9+";
 
 # Location of the remote and local XML files to track new releases.
 use constant REMOTE_FILE => 'http://updates.bugzilla.org/bugzilla-update.xml';

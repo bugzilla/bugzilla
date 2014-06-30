e2b27e46426ed1d32cc521229d920583b647fd7c
Byron Jones <glob@mozilla.com>
2014-06-30 21:53:44 +0800

    Bug 1030747: Recent changes to secure bugmail subject lines break GMail threading

--- extensions/SecureMail/Extension.pm
+++ extensions/SecureMail/Extension.pm
@@ -552,6 +552,8 @@ sub _make_secure {
         my $new = $add_new ? ' New:' : '';
         my $product = $email->header('X-Bugzilla-Product');
         my $component = $email->header('X-Bugzilla-Component');
+        # Note: the $bug_id is required within the parentheses in order to keep
+        # gmail's threading algorithm happy.
         $subject =~ s/($bug_id\])\s+(.*)$/$1$new (Secure bug $bug_id in $product :: $component)/;
         $email->header_set('Subject', $subject);
     }

ab1b842ba2e2f9ad3774c6c415129a4e196acc57
Dylan William Hardison <dylan@hardison.net>
2014-09-10 11:29:14 -0400

    Bug 1053513 - fix patching error
    I yearn for review board and more git-based development.

--- Bugzilla/Bug.pm
+++ Bugzilla/Bug.pm
@@ -1141,7 +1141,7 @@ sub update {
     my $last_visits =
       Bugzilla::BugUserLastVisit->match({ bug_id => $self->id });
     foreach my $lv (@$last_visits) {
-        $lv->remove_from_db() unless $lv->user->is_involved_with_bug($self);
+        $lv->remove_from_db() unless $lv->user->is_involved_in_bug($self);
     }
 
     # Update bug ignore data if user wants to ignore mail for this bug

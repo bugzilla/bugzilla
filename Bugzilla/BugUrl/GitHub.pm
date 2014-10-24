855faaab44d9e28349f7c9e3b45d7ea01eaaaf5d
Matt Selsky <selsky@columbia.edu>
2014-10-24 09:21:53 +0100

    Bug 1088399 - Add support for Github pull requests in See Also field. r=gerv, a=glob.

--- Bugzilla/BugUrl/GitHub.pm
+++ Bugzilla/BugUrl/GitHub.pm
@@ -22,8 +22,10 @@ sub should_handle {
 
     # GitHub issue URLs have only one form:
     #  https://github.com/USER_OR_TEAM_OR_ORGANIZATION_NAME/REPOSITORY_NAME/issues/111
+    # GitHub pull request URLs have only one form:
+    #  https://github.com/USER_OR_TEAM_OR_ORGANIZATION_NAME/REPOSITORY_NAME/pull/111
     return (lc($uri->authority) eq 'github.com'
-            and $uri->path =~ m|^/[^/]+/[^/]+/issues/\d+$|) ? 1 : 0;
+            and $uri->path =~ m!^/[^/]+/[^/]+/(?:issues|pull)/\d+$!) ? 1 : 0;
 }
 
 sub _check_value {

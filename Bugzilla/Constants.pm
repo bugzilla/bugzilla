243612ffd5bc1a16763348a2b1ea3a37a0852876
David Lawrence <dkl@mozilla.com>
2014-11-03 15:20:55 -0500

    Bump the version number to 5.1 on trunk

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -208,7 +208,7 @@ use Memoize;
 # CONSTANTS
 #
 # Bugzilla version
-use constant BUGZILLA_VERSION => "4.5.6+";
+use constant BUGZILLA_VERSION => "5.1";
 
 # A base link to the current REST Documentation. We place it here
 # as it will need to be updated to whatever the current release is.

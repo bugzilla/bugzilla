e4da3e9bd67ce8c306157eb07ce5c36997af1696
Koosha KM <koosha.khajeh@gmail.com>
2014-11-12 20:13:11 +0000

    Bug 1095758: Fix use of uninitialized value $hostname in Mailer.pm r=dkl,a=glob

--- Bugzilla/Mailer.pm
+++ Bugzilla/Mailer.pm
@@ -154,7 +154,7 @@ sub MessageToMTA {
         # address, but other mailers won't.
         my $urlbase = Bugzilla->params->{'urlbase'};
         $urlbase =~ m|//([^:/]+)[:/]?|;
-        $hostname = $1;
+        $hostname = $1 || 'localhost';
         $from .= "\@$hostname" if $from !~ /@/;
         $email->header_set('From', $from);
         

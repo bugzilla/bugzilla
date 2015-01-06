50ae5be2272d46780a6d0eab3912500a6615fac9
Byron Jones <glob@mozilla.com>
2015-01-06 12:07:46 +0800

    Bug 1117599: CVE ID format change: CVE-\d{4}-\d{4} becomes CVE-\d{4}-\d{4,} this year

--- extensions/BMO/Extension.pm
+++ extensions/BMO/Extension.pm
@@ -555,7 +555,7 @@ sub bug_format_comment {
 
     # link to CVE/CAN security releases
     push (@$regexes, {
-        match => qr/(?<!\/|=)\b((?:CVE|CAN)-\d{4}-(?:\d{4}|[1-9]\d{4,6})(?!\d))\b/,
+        match => qr/(?<!\/|=)\b((?:CVE|CAN)-\d{4}-(?:\d{4}|[1-9]\d{4,})(?!\d))\b/,
         replace => sub {
             my $args = shift;
             my $match = html_quote($args->{matches}->[0]);

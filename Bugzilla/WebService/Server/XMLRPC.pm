41a35f6dd3ac87d44dbdcc8418c57071cf6c5f37
Frédéric Buclin <LpSolit@gmail.com>
2015-01-07 14:33:16 +0100

    Bug 1113147: Revert the hack from bug 1108809 and blacklist SOAP::Lite 1.12 r=dkl a=glob

--- Bugzilla/WebService/Server/XMLRPC.pm
+++ Bugzilla/WebService/Server/XMLRPC.pm
@@ -47,13 +47,6 @@ BEGIN {
     };
 }
 
-sub new {
-    my $class = shift;
-    my $self = $class->SUPER::new(@_);
-    $self->{debug_logger} = sub {};
-    return $self;
-}
-
 sub initialize {
     my $self = shift;
     my %retval = $self->SUPER::initialize(@_);

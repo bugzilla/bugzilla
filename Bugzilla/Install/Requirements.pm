cd263e882459f87cf68b555764869c6bd0a52119
Frédéric Buclin <LpSolit@gmail.com>
2015-05-27 17:18:58 +0200

    Bug 1124401: Explicitly depend on DateTime::TimeZone::Local::Win32 on Windows r=dylan a=glob

--- Bugzilla/Install/Requirements.pm
+++ Bugzilla/Install/Requirements.pm
@@ -173,7 +173,8 @@ sub REQUIRED_MODULES {
     );
 
     if (ON_WINDOWS) {
-        push(@modules, {
+        push(@modules,
+        {
             package => 'Win32',
             module  => 'Win32',
             # 0.35 fixes a memory leak in GetOSVersion, which we use.
@@ -184,7 +185,14 @@ sub REQUIRED_MODULES {
             module  => 'Win32::API',
             # 0.55 fixes a bug with char* that might affect Bugzilla::RNG.
             version => '0.55',
-        });
+        },
+        {
+            package => 'DateTime-TimeZone-Local-Win32',
+            module  => 'DateTime::TimeZone::Local::Win32',
+            # We require DateTime::TimeZone 1.64, so this version must match.
+            version => '1.64',
+        }
+        );
     }
 
     my $extra_modules = _get_extension_requirements('REQUIRED_MODULES');

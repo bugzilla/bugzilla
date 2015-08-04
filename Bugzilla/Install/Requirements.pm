4b29b9f756aebf5c92110fe81261e9b81290e4c7
Gervase Markham <gerv@gerv.net>
2015-08-04 15:14:02 +0100

    Bug 1189737 - add File::Which to optional module requirements for building documentation. r=LpSolit

--- Bugzilla/Install/Requirements.pm
+++ Bugzilla/Install/Requirements.pm
@@ -452,7 +452,13 @@ sub OPTIONAL_MODULES {
         module  => 'File::Copy::Recursive',
         version => 0,
         feature => ['documentation'],
-    }
+    },
+    {
+        package => 'File-Which',
+        module  => 'File::Which',
+        version => 0,
+        feature => ['documentation'],
+    },
     );
 
     my $extra_modules = _get_extension_requirements('OPTIONAL_MODULES');

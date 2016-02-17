796b692bb18a9e1bcd1fa771d161b8bf3aef7c25
Gervase Markham <gerv@gerv.net>
2016-02-17 06:29:02 -0700

    Bug 1242264: Add LaTeX preamble to, speculatively, get ReadTheDocs to do 2 levels of heading in the TOC. r=LpSolit

--- docs/en/rst/conf.py
+++ docs/en/rst/conf.py
@@ -218,7 +218,7 @@ latex_elements = {
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+'preamble': '\setcounter{tocdepth}{1}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples

158015876e136343b474fc494231eac12c28a73f
Dylan William Hardison <dylan@hardison.net>
2016-09-11 19:29:00 -0400

    Bug 1128408 - migrate js/TUI.js and js/util.js to jquery

--- js/util.js
+++ js/util.js
@@ -307,16 +307,9 @@ function bz_fireEvent(anElement, anEvent) {
  * Adds a CSS class to an element if it doesn't have it. Removes the
  * CSS class from the element if the element does have the class.
  *
- * Requires YUI's Dom library.
- *
  * @param anElement  The element to toggle the class on
  * @param aClass     The name of the CSS class to toggle.
  */
 function bz_toggleClass(anElement, aClass) {
-    if (YAHOO.util.Dom.hasClass(anElement, aClass)) {
-        YAHOO.util.Dom.removeClass(anElement, aClass);
-    }
-    else {
-        YAHOO.util.Dom.addClass(anElement, aClass);
-    }
+    $(anElement).toggleClass(aClass);
 }

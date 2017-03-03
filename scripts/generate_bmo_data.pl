140cda81da78f1a95dc4159bdb2eae2a533b4fdf
Dylan William Hardison <dylan@hardison.net>
2017-03-03 14:51:55 -0500

    add persona back to user_info_class

--- scripts/generate_bmo_data.pl
+++ scripts/generate_bmo_data.pl
@@ -478,6 +478,8 @@ my %set_params = (
                                  '&amp;field0-0-0=bug_status&amp;type0-0-0=notequals' .
                                  '&amp;value0-0-0=UNCONFIRMED&amp;field0-0-1=reporter' .
                                  '&amp;type0-0-1=equals&amp;value0-0-1=%userid%',
+    persona_verify_url        => 'https://verifier.login.persona.org/verify',
+    persona_includejs_url     => 'https://login.persona.org/include.js',
     quip_list_entry_control   => 'moderated',
     restrict_comments_group   => 'editbugs',
     restrict_comments_enable_group => 'can_restrict_comments',
@@ -491,7 +493,7 @@ my %set_params = (
     usebugaliases             => 1,
     useqacontact              => 1,
     use_mailer_queue          => 1,
-    user_info_class           => 'CGI',
+    user_info_class           => 'Persona,CGI',
 );
 
 my $params_modified;

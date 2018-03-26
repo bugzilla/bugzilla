f76a9d7c96dbed50ee1fb9da56bcff818ea2eb5b
Dylan William Hardison <dylan@hardison.net>
2018-03-25 23:54:18 -0400

    no bug - TheSchwartz debug log is really more like info level in how we have used it

--- Bugzilla/JobQueue.pm
+++ Bugzilla/JobQueue.pm
@@ -101,7 +101,7 @@ sub debug {
     my $caller_pkg = caller;
     local $Log::Log4perl::caller_depth = $Log::Log4perl::caller_depth + 1;
     my $logger = Log::Log4perl->get_logger($caller_pkg);
-    $logger->debug(@args);
+    $logger->info(@args);
 }
 
 sub work {

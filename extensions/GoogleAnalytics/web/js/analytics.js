247125cc2033a204355105b4f7c4d12ff9b25c68
Kohei Yoshino <kohei.yoshino@gmail.com>
2019-08-06 12:51:23 -0400

    Bug 1529406 - Record user’s logged-in/out status and track insiders with Google Analytics

--- extensions/GoogleAnalytics/web/js/analytics.js
+++ extensions/GoogleAnalytics/web/js/analytics.js
@@ -5,18 +5,25 @@
  * This Source Code Form is "Incompatible With Secondary Licenses", as
  * defined by the Mozilla Public License, v. 2.0. */
 
-$(function() {
-  var meta = $('meta[name="google-analytics"]');
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
 
-  if (meta.length) {
-    // Activate Google Analytics
-    window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-    ga('create', meta.attr('content'), 'auto');
-    ga('set', 'anonymizeIp', true);
-    ga('set', 'location', meta.data('location'));
-    ga('set', 'title', meta.data('title'));
-    ga('set', 'transport', 'beacon');
-    // Track page view
-    ga('send', 'pageview');
+  const $meta = document.querySelector('meta[name="google-analytics"]');
+
+  if (!$meta) {
+    return;
   }
-});
+
+  // Activate Google Analytics
+  window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+  ga('create', $meta.content, 'auto');
+  ga('set', 'anonymizeIp', true);
+  ga('set', 'transport', 'beacon');
+  // Record a crafted location (template name) and title instead of actual URL
+  ga('set', 'location', $meta.dataset.location);
+  ga('set', 'title', $meta.dataset.title);
+  // Custom Dimension: logged in (true) or out (false)
+  ga('set', 'dimension1', !!BUGZILLA.user.login);
+  // Track page view
+  ga('send', 'pageview');
+}, { once: true });

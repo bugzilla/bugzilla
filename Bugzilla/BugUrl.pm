fb77856ed1937efe86ef05476a9564bb4d079035
dklawren <dklawren@users.noreply.github.com>
2020-06-01 22:34:59 -0400

    Bug 1641117 - Add Sentry to the list of See Also URLs.

--- Bugzilla/BugUrl.pm
+++ Bugzilla/BugUrl.pm
@@ -63,6 +63,7 @@ use constant SUB_CLASSES => qw(
   Bugzilla::BugUrl::JIRA
   Bugzilla::BugUrl::Trac
   Bugzilla::BugUrl::MantisBT
+  Bugzilla::BugUrl::MozSentry
   Bugzilla::BugUrl::SourceForge
   Bugzilla::BugUrl::GitHub
   Bugzilla::BugUrl::GitLab

7aefc2ffd033094ac1a1272fe34a82f32fc801dd
Dave Miller <github@justdave.net>
2021-10-13 16:41:04 -0400

    do not skip template compiling

--- docker/startup.sh
+++ docker/startup.sh
@@ -3,6 +3,7 @@
 cd /var/www/html
 apachectl start
 echo "Beginning checksetup..."
-perl checksetup.pl --no-template docker/checksetup_answers.txt
+perl checksetup.pl docker/checksetup_answers.txt
 echo "Checksetup completed."
+# don't exit docker
 while [ 1 ]; do sleep 1000; done

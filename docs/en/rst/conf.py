d9f234accdfb79d43520e76119475c0bf8f879fd
Andreas <development@meen.ch>
2022-09-06 00:40:45 +0200

    Use the bugzilla theme and the new logo

--- docs/en/rst/conf.py
+++ docs/en/rst/conf.py
@@ -128,7 +128,7 @@ them `here <https://bugzilla.mozilla.org/enter_bug.cgi?product=Bugzilla&componen
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'bugzilla'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -136,7 +136,7 @@ html_theme = 'default'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ["../themes"]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -145,11 +145,9 @@ html_theme = 'default'
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
 
-html_style = "bugzilla.css"
-
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "../images/bugzilla.png"
+html_logo = "../images/bugzilla.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -160,6 +158,12 @@ html_favicon = '../../../images/favicon.ico'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'custom.css',
+]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.

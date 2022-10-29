55121d55d71877dd9d6588e4df354e6f8ce8c5f4
Dylan Hardison <dylan@hardison.net>
2022-10-29 13:36:24 -0700

    add pod for Bugzilla::DB::QuoteIdentifier and Bugzilla::DB::Schema
    Bugzilla::DB::Quote::Identifier had no documentation previously,
    and it is now fully documented.
    
    Additionally, from the Moo-ification of Bugzilla::DB::Schema
    we lacked any docs for the new methods that were added for that change,
    and this is now fixed.

--- Bugzilla/DB/QuoteIdentifier.pm
+++ Bugzilla/DB/QuoteIdentifier.pm
@@ -67,5 +67,44 @@ as in the case of MySQL 8, formerly unreserved keywords can become reserved.
 This module provides a shortcut for quoting identifiers in strings by way of overloading a hash
 so that we can easily call C<quote_identifier> inside double-quoted strings.
 
+=head1 METHODS
+
+=head2 TIEHASH
+
+This class can be used as a tied hash, which is only done to allow quoting identifiers inside double-quoted strings.
+
+Exmaple:
+
+    my $qi = Bugzilla->dbh->qi;
+    my $sql = "SELECT $qi->{bug_id} FROM $qi->{bugs}";
+
+=head2 FETCH
+
+Returns the quoted identifier for the given key, this just calls C<quote_identifier> on the database handle.
+
+=head2 FIRSTKEY
+
+This returns nothing, as this tied hash has no keys or values.
+
+=head2 FIRSTVALUE
+
+This returns nothing, as this tied hash has no keys or values.
+
+=head2 EXISTS
+
+This always returns true, as this tied hash has no keys or values but technically every key exists.
+
+=head2 DELETE
+
+This always returns true, as this tied hash has no keys or values but technically every key can be deleted.
+
+=head2 db
+
+This is a weak reference to the database handle that is used to quote identifiers.
+
+=head1 SEE ALSO
+
+L<Bugzilla::DB::Schema>
+
 =cut
 

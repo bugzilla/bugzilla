f93cc9807383813713874bc88bcd95cd2708d8b4
Dave Miller <github@justdave.net>
2023-09-04 00:39:31 -0400

    Bug 1851354: blocklist MySQL 8+, require MySQL 5.6
    * Require MySQL 5.6.12 for InnoDB Fulltext support
    * Blocklist MySQL 8+ for groups issue
    * Independently require MariaDB 10+ for InnoDB fulltext support

--- Bugzilla/Constants.pm
+++ Bugzilla/Constants.pm
@@ -514,10 +514,13 @@ use constant INSTALLATION_MODE_NON_INTERACTIVE => 1;
 # Data about what we require for different databases.
 use constant DB_MODULE => {
 
-  # MySQL 5.0.15 was the first production 5.0.x release.
+  # Require MySQL 5.6.x for InnoDB's fulltext support
   'mysql' => {
     db         => 'Bugzilla::DB::Mysql',
-    db_version => '5.0.15',
+    db_version => '5.6.12',
+    db_blacklist => ['^[89]\.'],
+    # the following is a "human-readable" version to show in the release notes
+    db_blklst_str => '>= 8.0',
     dbd        => {
       package => 'DBD-mysql',
       module  => 'DBD::mysql',
@@ -530,6 +533,24 @@ use constant DB_MODULE => {
       version => '4.001',
     },
     name => 'MySQL'
+  },
+
+  # MariaDB needs version 10 for InnoDB fulltext support
+  'mariadb' => {
+    db         => 'Bugzilla::DB::Mysql',
+    db_version => '10.0',
+    dbd        => {
+      package => 'DBD-mysql',
+      module  => 'DBD::mysql',
+
+      # Disallow development versions
+      blacklist => ['_'],
+
+      # For UTF-8 support. 4.001 makes sure that blobs aren't
+      # marked as UTF-8.
+      version => '4.001',
+    },
+    name => 'MariaDB'
   },
 
   # Also see Bugzilla::DB::Pg::bz_check_server_version, which has special

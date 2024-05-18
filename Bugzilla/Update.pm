46f209a699b49e559ce94c9e5fa4519638056734
Dave Miller <justdave@bugzilla.org>
2024-05-18 19:52:58 -0400

    fix tests

--- Bugzilla/Update.pm
+++ Bugzilla/Update.pm
@@ -48,7 +48,7 @@ sub get_notifications {
             'status'     => $branch->{'att'}->{'status'},
             'url'        => $branch->{'att'}->{'url'},
             'date'       => $branch->{'att'}->{'date'},
-	    'eos_date'   => exists($branch->{'att'}->{'eos-date'}) ? $branch->{'att'}->{'eos-date'} : undef,
+            'eos_date'   => exists($branch->{'att'}->{'eos-date'}) ? $branch->{'att'}->{'eos-date'} : undef,
         };
         push(@releases, $release);
     }
@@ -94,8 +94,8 @@ sub get_notifications {
             return {'data' => $release[0], 'deprecated' => $branch_version};
         }
 
-	# If we get here, then we want to recommend the lastest stable
-	# release without any other messages.
+        # If we get here, then we want to recommend the lastest stable
+        # release without any other messages.
         @release = grep {$_->{'status'} eq 'stable'} @releases;
     }
     elsif (Bugzilla->params->{'upgrade_notification'} eq 'stable_branch_release') {
@@ -110,14 +110,14 @@ sub get_notifications {
         # If the branch has an end-of-support date listed, we should
         # strongly suggest to upgrade to the latest stable release
         # available.
-	if (scalar(@release) && $release[0]->{'status'} ne 'closed'
-	    && defined($release[0]->{'eos_date'})) {
+       if (scalar(@release) && $release[0]->{'status'} ne 'closed'
+            && defined($release[0]->{'eos_date'})) {
             my $eos_date = $release[0]->{'eos_date'};
             @release = grep {$_->{'status'} eq 'stable'} @releases;
             return {'data' => $release[0],
                     'branch_version' => $branch_version,
-                    'eos_date' => $eos_date};
-	};
+                    'eos_date' => $eos_date}
+        };
 
         # If the branch is now closed, we should strongly suggest
         # to upgrade to the latest stable release available.

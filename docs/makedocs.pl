1277e856de1f5cffbc8ead8a821adb4d182f886a
Dave Miller <justdave@bugzilla.org>
2024-08-25 23:17:21 -0400

    Bug 1849943: get doc build working on RTD site (#140)

--- docs/makedocs.pl
+++ docs/makedocs.pl
@@ -33,9 +33,10 @@ BEGIN { chdir dirname($0); }
 use lib qw(.. ../lib lib ../local/lib/perl5);
 
 use Cwd;
+use File::Copy qw(copy);
 use File::Copy::Recursive qw(rcopy);
 use File::Find;
-use File::Path qw(rmtree);
+use File::Path qw(rmtree make_path);
 use File::Which qw(which);
 use Pod::Simple;
 
@@ -84,13 +85,19 @@ END_HTML
 
   $converter->contents_page_start($contents_start);
   $converter->contents_page_end("</body></html>");
-  $converter->add_css('./../../../style.css');
+  if (exists($::ENV{'READTHEDOCS'})) {
+    $converter->add_css('./../../style.css');
+  } else {
+    $converter->add_css('./../../../../style.css');
+  }
   $converter->javascript_flurry(0);
   $converter->css_flurry(0);
-  mkdir("html");
-  mkdir("html/api");
-  $converter->batch_convert(['../../'], 'html/api/');
+  make_path('html/integrating/api');
+  $converter->batch_convert(['../../'], 'html/integrating/api');
 
+  if (exists($::ENV{'READTHEDOCS'})) {
+    copy('../style.css', 'html/style.css') or die "Copy failed: $!";
+  }
   print "\n";
 }
 

28abcf53d46dd46257eb4fe3c0520f2601cb1e22
Frédéric Buclin <LpSolit@gmail.com>
2026-07-25 15:52:44 -0400

    Bug 2057695: fix precedent issues with negation
    r=justdave

--- Bugzilla/Bug.pm
+++ Bugzilla/Bug.pm
@@ -587,7 +587,7 @@ sub possible_duplicates {
   my $products   = $params->{products} || [];
   my $limit      = $params->{limit} || MAX_POSSIBLE_DUPLICATES;
   $limit    = MAX_POSSIBLE_DUPLICATES if $limit > MAX_POSSIBLE_DUPLICATES;
-  $products = [$products]             if !ref($products) eq 'ARRAY';
+  $products = [$products]             if ref($products) ne 'ARRAY';
 
   my $orig_limit = $limit;
   detaint_natural($limit)
